ab9e0cd3839c1c1056c4291150906b170fa90faa
El Giancar <giancarmine.cuofano@gmail.com>
2020-02-02 12:22:06 +0100

    Setup spring boot project

---
+++ src/main/java/org/ges/rytest/RytestApplication.java
@@ -0,0 +1,13 @@
+package org.ges.rytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RytestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ges/rytest/RytestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ges.rytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
